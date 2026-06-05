b009ccca6d3e288a986ad48ca799eac55412cbe1
Tristan Millington <tristan.millington@gmail.com>
2026-06-05 10:45:58 +0100

    added initial tests of keycloak setup

---
+++ app/test_main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import pytest
+
+from app.api import verify_token
+from .main import app
+
+client = TestClient(app)
+
+def mock_verify_token():
+    return {"preferred_username": "testuser", "realm_access": {"roles": ["user"]}}
+
+@pytest.fixture
+def authed_client():
+    app.dependency_overrides[verify_token] = lambda: {"preferred_username": "testuser"}
+    yield TestClient(app)
+    app.dependency_overrides.clear()
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+def test_get_no_token():
+    response = client.get("/egress/1")
+    assert response.status_code == 401
+
+def test_put_no_token():
+    response = client.put("/egress/1")
+    assert response.status_code == 401
+
+def test_protected_get_with_invalid_token():
+    response = client.get("/egress/1", headers={"Authorization": "Bearer sometoken"})
+    assert response.status_code == 401
+
+def test_protected_get_with_valid_token(authed_client):
+    response = authed_client.get("/egress/1")
+    assert response.status_code == 200
