e28a30227f2ff0e3a3f35917e286d0d81834281d
Tristan Millington <tristan.millington@gmail.com>
2026-06-15 16:55:04 +0100

    added an integration test for approving a file

--- app/tests/test_main_integrations.py
+++ app/tests/test_main_integrations.py
@@ -29,3 +29,32 @@ def test_egress_get_live(authed_client):
     token = jwt.encode(dct, settings.secret_key)
     response = authed_client.get(f"/egress/{token}")
     assert response.status_code == 200
+
+@pytest.mark.integration
+def test_egress_approve_live(authed_client):
+    """
+    Test we can approve a file and see said approvals and comments
+    """
+    project_id = "1"
+    dct = {"projectId": project_id, "userId": "user1", "bucketId": "test-bucket"}
+    token = jwt.encode(dct, settings.secret_key)
+    response = authed_client.get(f"/egress/{token}")
+    assert response.status_code == 200
+    
+    res = response.json()
+    assert len(res) > 0
+    file_id = res[0]["id"]
+    body = {file_id: {"status": "approve", "comment": "example"}}
+    response = authed_client.put(f"/egress/{token}", json=body)
+    assert response.status_code == 200
+
+    response = authed_client.get(f"/egress/{token}")
+    assert response.status_code == 200
+    res = response.json()
+    assert len(res[0]["approvals"]) > 0
+    # assert res["approvals"][0]["comment"] == "example"
+
+    # Delete the approval
+    body = {file_id: {"status": "reject", "comment": ""}}
+    response = authed_client.put(f"/egress/{token}", json=body)
+    assert response.status_code == 200
